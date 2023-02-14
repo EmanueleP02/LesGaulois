fcfd384227c177df964ee508d8055cf7bc2ecb40
pigani emanuele <pgm4040a@ens.fsi>
2023-02-14 19:44:41 +0100

    fin cours TP

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -8,25 +8,26 @@ public class Gaulois {
 		this.nom = nom;
 		this.force = force;
 }
-public String getNom() {
-	return nom;
-}
-public void parler(String texte) {
-	System.out.println(prendreParole() + "« " + texte + "»");
-}
-private String prendreParole() {
-	return "Le gaulois " + nom + " : ";
-}
-public void frapper(Romain romain) {
-	System.out.println(nom + " envoie un grand coup dans la mâchoire de "+ romain.getNom());
-	romain.recevoirCoup(force / 3);
-}
-@Override
-public String toString() {
-	return "Gaulois [nom=" + nom + ", force=" + force
-			+ ", effetPotion=" + effetPotion + "]";
-}
-public static void main(String[] args) {
-//TODO créer un main permettant de tester la classe Gaulois
-}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "+ romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+	@Override
+//	public String toString() {
+//		return "Gaulois [nom=" + nom + ", force=" + force
+//				+ ", effetPotion=" + effetPotion + "]";
+//	}
+	public static void main(String[] args) {
+		Gaulois asterix (nom = "Astérix", force = 8);
+		System.out.println(asterix);
+	}
 }
